df7ad3bc5161a244696b71a5bcd8f835325f727f
Daniel Tavares <daniel.tavares@lnls.br>
2024-06-13 06:32:51 -0300

    regrid_hexa_data.m: fix wrong number of hexas

--- regrid_hexa_data.m
+++ regrid_hexa_data.m
@@ -4,10 +4,10 @@ if nargin < 3
     interp_method = 'nearest';
 end
 
-ny_hexa = detector.chip_array(2)/detector.hexa_array(2);
+nx_hexa = detector.chip_array(1)/detector.hexa_array(1);
 
 xregrid = -(detector.px_array(1)-1)/2:(detector.px_array(1)-1)/2;
-yregrid = -((ny_hexa-1)*detector.chip_gap + ny_hexa*detector.px_array(2)-1)/2:((ny_hexa-1)*detector.chip_gap + ny_hexa*detector.px_array(2)-1)/2;
+yregrid = -((nx_hexa-1)*detector.chip_gap + nx_hexa*detector.px_array(2)-1)/2:((nx_hexa-1)*detector.chip_gap + nx_hexa*detector.px_array(2)-1)/2;
 [xrealmesh, yrealmesh] = meshgrid(xregrid*cos(detector.hexa_tilt),yregrid);
 [xregridmesh, yregridmesh] = meshgrid(xregrid,yregrid);
 for i=1:numel(hexa_data)
